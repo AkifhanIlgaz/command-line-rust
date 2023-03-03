6f2948d51feefdf4daebdd451d0a5b9cb55801c4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-03-03 12:09:15 +0300

    add some tests

--- echo_rust/tests/cli.rs
+++ echo_rust/tests/cli.rs
@@ -14,12 +14,32 @@ fn dies_no_args() -> TestResult {
     Ok(())
 }
 
-fn run(args: &[&str], expected_file: &str) -> TestResult {
-    let expected = fs::read_to_string(expected_file)?;
+fn run(args: &[&str], path_of_expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(path_of_expected_file)?;
     Command::cargo_bin("echo_rust")?
         .args(args)
         .assert()
         .success()
         .stdout(expected);
     Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
